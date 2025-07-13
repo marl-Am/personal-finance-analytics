3942c60f178e9ad387fbe581d8155fb9e7d1a848
marl-Am <marl-Am@users.noreply.github.com>
2025-07-13 16:52:00 -0400

    added terms
    - Added terms and privacy pages
    - Disabled "forgot password" on login.html
    - Disabled "Email Preferences", "Privacy Settings", and "Data Usage"
    - Disabled "Export Data - Download all your financial data in CSV format"

--- app.py
+++ app.py
@@ -112,5 +112,15 @@ def dashboard():
     )
 
 
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
